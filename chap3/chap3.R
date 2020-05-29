cabec371182994865777d260a5e4a5b1fcf413cb
J <jkelleypga@gmail.com>
2020-05-28 21:50:50 -0500

    finished chap 3 readings

--- chap3/chap3.R
+++ chap3/chap3.R
@@ -44,3 +44,126 @@ autoplot(goog200) +
   guides(colour=guide_legend(title="Forecast"))
 
 # 3.2
+
+dframe <- cbind(Monthly = milk,
+                DailyAverage = milk/monthdays(milk))
+autoplot(dframe, facet=TRUE) +
+  xlab("Years") + ylab("Pounds") +
+  ggtitle("Milk production per cow")
+
+(lambda <- BoxCox.lambda(elec))
+autoplot(BoxCox(elec,lambda))
+
+# Forecast returns median when you do box cox, if mean is desired select biasadjustment
+fc <- rwf(eggs, drift=TRUE, lambda=0, h=50, level=80)
+fc2 <- rwf(eggs, drift=TRUE, lambda=0, h=50, level=80,
+           biasadj=TRUE)
+autoplot(eggs) +
+  autolayer(fc, series="Simple back transformation") +
+  autolayer(fc2, series="Bias adjusted", PI=FALSE) +
+  guides(colour=guide_legend(title="Forecast"))
+
+# 3.3
+#Fitted values always involve one-step forecasts.
+# if there are any parameters which are estimated we  aren'
+autoplot(goog200) +
+  xlab("Day") + ylab("Closing Price (US$)") +
+  ggtitle("Google Stock (daily ending 6 December 2013)")
+
+res <- residuals(naive(goog200))
+
+autoplot(res) + xlab("Day") + ylab("") +
+  ggtitle("Residuals from naïve method")
+
+gghistogram(res) + ggtitle("Histogram of residuals")
+
+ggAcf(res) + ggtitle("ACF of residuals")
+
+# lag=h and fitdf=K
+Box.test(res, lag=10, fitdf=0)
+
+
+Box.test(res,lag=10, fitdf=0, type="Lj")
+# testing if Q* comes from a white noise process.
+checkresiduals(naive(goog200))
+
+
+# 3.4 - training/testing sets
+window(ausbeer, start=1995)
+
+# subsetting can use indexes or time periods
+subset(ausbeer, start=length(ausbeer)-4*5)
+subset(ausbeer, quarter = 1)
+tail(ausbeer, 4*5)
+
+#minimizing to MAE yields the median while minimizing to RMSE yields Mean
+
+beer2 <- window(ausbeer,start=1992,end=c(2007,4))
+beerfit1 <- meanf(beer2,h=10)
+beerfit2 <- rwf(beer2,h=10)
+beerfit3 <- snaive(beer2,h=10)
+autoplot(window(ausbeer, start=1992)) +
+  autolayer(beerfit1, series="Mean", PI=FALSE) +
+  autolayer(beerfit2, series="Naïve", PI=FALSE) +
+  autolayer(beerfit3, series="Seasonal naïve", PI=FALSE) +
+  xlab("Year") + ylab("Megalitres") +
+  ggtitle("Forecasts for quarterly beer production") +
+  guides(colour=guide_legend(title="Forecast"))
+
+beer3 <- window(ausbeer, start=2008)
+accuracy(beerfit1, beer3)
+accuracy(beerfit2, beer3)
+accuracy(beerfit3, beer3)
+
+googfc1 <- meanf(goog200, h=40)
+googfc2 <- rwf(goog200, h=40)
+googfc3 <- rwf(goog200, drift=TRUE, h=40)
+autoplot(subset(goog, end = 240)) +
+  autolayer(googfc1, PI=FALSE, series="Mean") +
+  autolayer(googfc2, PI=FALSE, series="Naïve") +
+  autolayer(googfc3, PI=FALSE, series="Drift") +
+  xlab("Day") + ylab("Closing Price (US$)") +
+  ggtitle("Google stock price (daily ending 6 Dec 13)") +
+  guides(colour=guide_legend(title="Forecast"))
+
+googtest <- window(goog, start=201, end=240)
+accuracy(googfc1, googtest)
+accuracy(googfc2, googtest)
+accuracy(googfc3, googtest)
+
+# Time Series CV
+e <- tsCV(goog200, rwf, drift=TRUE, h=1)
+sqrt(mean(e^2, na.rm=TRUE))
+sqrt(mean(residuals(rwf(goog200, drift=TRUE))^2, na.rm=TRUE))
+
+goog200 %>% tsCV(forecastfunction=rwf, drift=TRUE, h=1) -> e
+e^2 %>% mean(na.rm=TRUE) %>% sqrt()
+goog200 %>% rwf(drift=TRUE) %>% residuals() -> res
+res^2 %>% mean(na.rm=TRUE) %>% sqrt()
+
+e <- tsCV(goog200, forecastfunction=naive, h=8)
+# Compute the MSE values and remove missing values
+mse <- colMeans(e^2, na.rm = T)
+# Plot the MSE values against the forecast horizon
+data.frame(h = 1:8, MSE = mse) %>%
+  ggplot(aes(x = h, y = MSE)) + geom_point()
+
+
+###############################################################
+#
+# 3.5 Prediction Intervals
+#
+###############################################################
+naive(goog200)
+
+autoplot(naive(goog200))
+
+naive(goog200, bootstrap=TRUE)
+
+###############################################################
+#
+# 3.6 FOrecast Package
+#
+###############################################################
+
+forecast(ausbeer, h=4)
